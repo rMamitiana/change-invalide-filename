cb2842712d98af6ba8800394fec67d25881f5f6e
rMamitiana <tnatimima@gmail.com>
2022-02-14 06:27:02 +0300

    first test

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import re
 import sys
 
 list = []
-PATH = './dir1/'
-reg_ex = r'[0-9_:]'
+PATH = './toChange/'
+'''invalide characters list: <>:"/\|?*'''
+reg_ex = r'[<>:"/\\|?*]'
+error = 0
 
 '''
 Get all directories, subdirectories and files in the toChange directory to list
@@ -24,28 +26,32 @@ for i in range(0, length):
     files = level[2]
     print(root, dirs, files)
     
-    for dir in dirs:
+    if root != PATH:
+        root_path = root + "/"
+    else:
         root_path = root
-        if root != PATH:
-            root_path = root + "/"
+
+    for dir in dirs:
         try:
             print('\nold_name: ', root_path + dir)
             print('new_name: ', root_path + re.sub(reg_ex, '', dir))
             os.rename(root_path + dir, root_path + re.sub(reg_ex, '', dir))
         except:
+            error = error + 1
             print("Exception occured : ", sys.exc_info())
 
     for file in files:
-        root_path = root
-        if root != PATH:
-            root_path = root + "/"
         try:
             print('\nold_name: ', root_path + file)
             print('new_name: ', root_path + re.sub(reg_ex, '', file))
             os.rename(root_path + file,  root_path + re.sub(reg_ex, '', file))
         except:
+            error = error + 1
             print("Exception occured : ", sys.exc_info())
+
     print("\n ============================================= \n")
 
-# txt = 'Man:de/ana ve ny ma:nd_e r:a/ atao an\'zao?'
-# print(re.sub(reg_ex, '', txt))
+if error == 0:
+    print("Operations finished succefully !")
+else:
+    print("Operations finished with ", error , " errros !")
